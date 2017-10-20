2174ced6cc76e3e8313d90f7d88f509af006d5fd
Kunal Garg <kunalgarg2100@gmail.com>
2017-10-20 22:41:35 +0530

    Added maxunlock and error handlers

--- src/QuestionViewer/index.js
+++ src/QuestionViewer/index.js
@@ -11,21 +11,51 @@ var md = new Remarkable({
 
 class QuestionViewer extends Component {
   state = {
+    maxQuestion : '',
+    maxUnlocked : '', 
     question: {},
     loading: true,
     error: '',
+    status: '',
   }
   async componentDidMount() {
     await this.fetchQuestion()
+
   }
   async componentWillReceiveProps(nextProps) {
     await this.fetchQuestion(nextProps.params.qno)
   }
   async fetchQuestion(qno = this.props.params.qno) {
     var res = await window.fetchWithAuth(`/questions/${qno}`);
+    if(res.status!==200){
+      if (!res.error) this.setState({ status: res.status, loading: false })
+      else this.setState({ error: res.error, loading: false })
+    }
+    
     res = await res.json();
+
+    var res1 = await window.fetchWithAuth(`/users`);
+    res1 = await res1.json();
+
+    var user_id;
+    for(var i =0 ; i< res1.length ; i++)
+      if(res1[i].email === window.email)
+        {
+          user_id = res1[i].id;
+          break;
+        }
+    var res2 = await window.fetchWithAuth(`/users/${user_id}`);
+    res2 = await res2.json();
+    
+    var res3 = await window.fetchWithAuth('/questions');
+    res3 = await res3.json();
+    if (!res3.error) this.setState({ maxQuestion: res3.length, loading: false })
+    else this.setState({ error: res3.error, loading: false })
+
     if (!res.error) this.setState({ question: res, loading: false })
     else this.setState({ error: res.error, loading: false })
+    if (!res2.error) this.setState({ maxUnlocked: res2.maxUnlock, loading: false })
+    else this.setState({ error: res2.error, loading: false })
   }
   checkAnswer = e => {
     e.preventDefault();
@@ -41,8 +71,17 @@ class QuestionViewer extends Component {
   }
 
   render() {
-    const { loading, question, error, answer } = this.state
+    const { loading, question, error, answer , maxUnlocked, maxQuestion, status} = this.state
     const qno = parseInt(this.props.params.qno,10)
+     if(status === 403)
+    return(
+    <h1 style="text-align:center;">403 Forbidden</h1>
+    )
+  if(status === 404)
+    return(
+    <h1 style="text-align:center;">404 Page Not Found</h1>
+    )
+    else
     return (
       <div>
         {loading && <div>Loading...</div>}
@@ -64,13 +103,14 @@ class QuestionViewer extends Component {
             </Link>
         }
         {
-          qno!==5 &&
+          qno!== maxQuestion && qno < maxUnlocked &&
             <Link className="button float-right" to={`/question/${qno+1}`}>
               Next
             </Link>
         }
       </div>
     )
+ 
   }
 }
 
